c7695210096228590955ecfc90d796a6205494f5
Saketh811 <65438817+Saketh811@users.noreply.github.com>
2021-06-12 18:02:16 +0530

    Visual Commit

--- App.js
+++ App.js
@@ -56,7 +56,7 @@ export default function App() {
         <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)} />
         <TouchableOpacity onPress={() => handleAddTask()}>
           <View style={styles.addWrapper}>
-            <Text style={styles.addText}>+</Text>
+            <Text style={styles.addText}>></Text>
           </View>
         </TouchableOpacity>
       </KeyboardAvoidingView>
@@ -93,16 +93,16 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 15,
     backgroundColor: '#FFF',
-    borderRadius: 60,
+    borderRadius: 10,
     borderColor: '#C0C0C0',
     borderWidth: 1,
-    width: 250,
+    width: 350,
   },
   addWrapper: {
     width: 60,
     height: 60,
-    backgroundColor: '#FFF',
-    borderRadius: 60,
+    backgroundColor: '#808080',
+    borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
     borderColor: '#C0C0C0',
